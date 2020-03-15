3cff7c9b9cc81ba769878e2692d7db16dd91d8c8
Joseph-Davies <37886405+Joseph-Davies@users.noreply.github.com>
2020-03-15 21:26:01 +1100

    Create create-folders.sh

---
+++ create-folders.sh
@@ -0,0 +1,11 @@
+sudo mkdir /data
+sudo mkdir /personalisation
+sudo mkdir /app_image
+
+sudo chmod 755 /data
+sudo chmod 755 /personalisation
+sudo chmod 755 /app_image
+
+sudo chown joseph /data
+sudo chown joseph /personalisation
+sudo chown joseph /app_image
