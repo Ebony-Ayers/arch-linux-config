eeeb30780b974479e6098f1bf7e5f6ee1d001834
Joseph-Davies <37886405+Joseph-Davies@users.noreply.github.com>
2020-03-29 14:18:06 +1100

    Update create-folders.sh

--- create-folders.sh
+++ create-folders.sh
@@ -1,11 +1,14 @@
 sudo mkdir /data
 sudo mkdir /personalisation
 sudo mkdir /app_image
+sudo mkdir /vulkan
 
 sudo chmod 755 /data
 sudo chmod 755 /personalisation
 sudo chmod 755 /app_image
+sudo chmod 755 /vulkan
 
 sudo chown joseph /data
 sudo chown joseph /personalisation
 sudo chown joseph /app_image
+sudo chown joseph /vulkan
