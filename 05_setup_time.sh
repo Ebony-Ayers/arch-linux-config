db85d35d82b282f5627a4e126994a0ba2b09837d
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-04-20 16:53:16 +1000

    Update 05_setup_time.sh

--- 05_setup_time.sh
+++ 05_setup_time.sh
@@ -1,4 +1,4 @@
-ln -sf $1 /etc/localtime
+ln -sf ${1:/usr/share/zoneinfo/Australia/Melbourne} /etc/localtime
 hwclock --systohc
 sed -i "s|#en_AU.UTF-8 UTF-8|en_AU.UTF-8 UTF-8|g" /etc/locale.gen
 sed -i "s|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g" /etc/locale.gen
