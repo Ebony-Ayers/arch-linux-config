1883b4a4e866c043bb332ba7133f747ee67d238b
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-04-20 16:45:55 +1000

    Update 05_setup_time.sh

--- 05_setup_time.sh
+++ 05_setup_time.sh
@@ -2,5 +2,5 @@ ln -sf $1 /etc/localtime
 hwclock --systohc
 sed -i "s|#en_AU.UTF-8 UTF-8|en_AU.UTF-8 UTF-8|g" /etc/locale.gen
 sed -i "s|#en_US.UTF-8 UTF-8|en_US.UTF-8 UTF-8|g" /etc/locale.gen
-locale.gen
+locale-gen
 echo "LANG=en_AU.UTF-8" >> /etc/locale.conf
