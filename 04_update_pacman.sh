e14e6b70b80d07ddfa5bb8827d9a53a225fabbaf
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-04-20 16:36:16 +1000

    Update 04_update_pacman.sh

--- 04_update_pacman.sh
+++ 04_update_pacman.sh
@@ -1,4 +1,4 @@
-sed -i "s|#\[multilib\]|\[multilib\]|g" etc/pacman.conf
-sed -i "s|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g" etc/pacman.conf
+sed -i "s|#\[multilib\]|\[multilib\]|g" /etc/pacman.conf
+sed -i "s|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g" /etc/pacman.conf
 pacman -Syyuu
 pacman -S git nano
