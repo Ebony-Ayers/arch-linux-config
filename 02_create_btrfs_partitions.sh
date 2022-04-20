2e35b987e29580b8608c1c8b7ad7a9031aa0c4d5
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-04-20 16:21:12 +1000

    Update 02_create_btrfs_partitions.sh

--- 02_create_btrfs_partitions.sh
+++ 02_create_btrfs_partitions.sh
@@ -7,4 +7,4 @@ mount -t btrfs -o subvol=/@root $1 /mnt
 mkdir /mnt/home
 mkdir /mnt/.snapshots
 mount -t btrfs -o subvol=/@home $1 /mnt/home
-mount -t btrfs -o subvol=/@shapshots $1 /mnt/home
+mount -t btrfs -o subvol=/@shapshots $1 /mnt/.snapshots
