af91d182479613744f8674b6bea59b4d81396455
Ebony Ayers <ebonyayers2718@gmail.com>
2022-06-14 03:34:10 +1000

    fixed issue with sed uncommenting all lines in 04

--- 04_update_pacman.sh
+++ 04_update_pacman.sh
@@ -1,4 +1,5 @@
-sed -i "s|#\[multilib\]|\[multilib\]|g" /etc/pacman.conf
-sed -i "s|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g" /etc/pacman.conf
+sed -i -z "s/\n/|/g" ~/pacman.conf
+sed -i "s+#\[multilib\]|#Include = /etc/pacman.d/mirrorlist+\[multilib\]|Include = /etc/pacman.d/mirrorlist+g" ~/pacman.conf
+sed -i -z "s/|/\n/g" ~/pacman.conf
 pacman -Syyuu
 pacman -S git nano
