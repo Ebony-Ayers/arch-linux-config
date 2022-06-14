14e57cf1285fe47a9dbf902c477be168af054963
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-06-15 01:41:23 +1000

    Update 10_users.sh

--- 10_users.sh
+++ 10_users.sh
@@ -1,4 +1,10 @@
+if [ $# -eq 0 ]
+then
+  echo "Error: no partition supplied"
+  exit
+fi
+
 export EDITOR=nano
 visudo
 useradd -m $1
-usermod -aG -G video,storage,optical,audio,wheel $1
+usermod -aG video,storage,optical,audio,wheel $1
