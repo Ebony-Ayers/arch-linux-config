c19c3c9d68df4581566d22064457c6bf598eb2e1
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-06-15 01:33:42 +1000

    Update 04_update_pacman.sh

--- 04_update_pacman.sh
+++ 04_update_pacman.sh
@@ -2,4 +2,4 @@ sed -i -z "s/\n/|/g" /etc/pacman.conf
 sed -i "s+#\[multilib\]|#Include = /etc/pacman.d/mirrorlist+\[multilib\]|Include = /etc/pacman.d/mirrorlist+g" /etc/pacman.conf
 sed -i -z "s/|/\n/g" /etc/pacman.conf
 pacman -Syyuu
-pacman -S git nano
+pacman -S nano dosfstools btrfs-progs
