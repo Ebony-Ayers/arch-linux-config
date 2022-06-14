fee86619749b55eaa4ac7c32cc03ccd4098a83b1
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-06-15 01:38:02 +1000

    Update 08_grub.sh

--- 08_grub.sh
+++ 08_grub.sh
@@ -1,3 +1,9 @@
+if [ $# -eq 0 ]
+then
+  echo "Error: no grub partition supplied"
+  exit
+fi
+
 pacman -S grub efibootmgr
 mount $1 /mnt
 grub-install --target=x86_64-efi --efi-directory=/mnt --bootloader-id=GRUB
