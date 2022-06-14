1d47cf962ceb0b1617ecfb304304f12331990227
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2022-06-15 01:20:12 +1000

    Update 06_machine_name_and_network.sh

--- 06_machine_name_and_network.sh
+++ 06_machine_name_and_network.sh
@@ -1,6 +1,13 @@
+
+if [ $# -eq 0 ]
+then
+  echo "Error: no machine name provided"
+  exit
+fi
+
 echo $1 >> /etc/hostname
 echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1		localhost" >> /etc/hosts
-echo "127.0.1.1	$1.localdomain $1" >> etc/hosts
+echo "127.0.1.1	$1.localdomain $1" >> /etc/hosts
 pacman -S networkmanager
 systemctl enable NetworkManager.service
