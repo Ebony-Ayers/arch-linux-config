533d68ceefe4dfacf42814f53a99732066c41fc1
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2023-01-28 10:31:15 +0000

    Update 11_users.sh

--- 11_users.sh
+++ 11_users.sh
@@ -8,7 +8,7 @@ export EDITOR=nano
 visudo
 useradd -m $1
 usermod -aG video,storage,optical,audio,wheel $1
-echo "set password for $1"
+echo "set password for $1 (this is the user account, root password is next)"
 passwd $1
 echo "set password for root"
 passwd
