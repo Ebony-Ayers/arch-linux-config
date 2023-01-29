32687ea8c42fff105901304d3dae13bb325917aa
Ebony Ayers <37886405+Ebony-Ayers@users.noreply.github.com>
2023-01-29 19:14:22 +1100

    Update 06_btrfs_update_pacman.sh

--- 06_btrfs_update_pacman.sh
+++ 06_btrfs_update_pacman.sh
@@ -2,4 +2,4 @@ sed -i -z "s/\n/|/g" /etc/pacman.conf
 sed -i "s+#\[multilib\]|#Include = /etc/pacman.d/mirrorlist+\[multilib\]|Include = /etc/pacman.d/mirrorlist+g" /etc/pacman.conf
 sed -i -z "s/|/\n/g" /etc/pacman.conf
 pacman -Syyuu
-pacman -S nano dosfstools btrfs-progs gcc make cmake ccache base-devel python
+pacman -S nano dosfstools btrfs-progs gcc make cmake ccache base-devel python python-pip
