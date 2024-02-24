c7e06605750333f6690d5bfe0e8dd7943b9db501
ebony-ayers <ebonyayers2718@gmail.com>
2024-02-24 22:56:55 +1100

    added alsa packages

--- legacy/06_ext4_update_pacman.sh
+++ legacy/06_ext4_update_pacman.sh
@@ -2,4 +2,4 @@ sed -i -z "s/\n/|/g" /etc/pacman.conf
 sed -i "s+#\[multilib\]|#Include = /etc/pacman.d/mirrorlist+\[multilib\]|Include = /etc/pacman.d/mirrorlist+g" /etc/pacman.conf
 sed -i -z "s/|/\n/g" /etc/pacman.conf
 pacman -Syyuu
-pacman -S nano dosfstools e2fsprogs base-devel --noconfirm
+pacman -S nano dosfstools e2fsprogs base-devel alsa-utils alsa-firmware --noconfirm
